53b0fc45565ac1cd255fc2f7cc70d5dc4dde7f4c
Witcher3k <witcherek1112@gmail.com>
2018-01-18 19:14:30 +0100

    Test

---
+++ suwak_poziom_jasnosci/script.js
@@ -0,0 +1,11 @@
+var input =document.getElementById("input");
+input.addEventListener("change",updateValue);
+input.addEventListener("mousemove",updateValue);
+
+function updateValue(){
+    console.log(this.value);
+    var percent = document.querySelector("span");
+    percent.textContent=this.value;
+    document.documentElement.style.setProperty("--light",this.value/100);
+    document.documentElement.style.setProperty("--glow",this.value/4 +"px");
+}
